807510fc0b375813c6abf9d8d059bacae8e505d9
irezwi <zwirek.ireneusz@gmail.com>
2021-05-24 16:51:04 +0200

    #PLANT-42: Add Notes model #PLANT-44: Provide permission classes to limit object access to not owned objects

--- plantapp/backend/admin.py
+++ plantapp/backend/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Plant, Location, Reminder
+from .models import Plant, Location, Reminder, Note
 
 admin.site.register(Plant)
 admin.site.register(Location)
 admin.site.register(Reminder)
+admin.site.register(Note)
